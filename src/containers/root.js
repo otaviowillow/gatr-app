16b78e64a26cca7aaf01bcf3d656febfbb453fa1
Otavio Padovani <otaviopadovani@Otavios-MacBook-Pro.local>
2018-05-03 21:39:44 -0700

    Trying to build t prod

--- src/containers/root.js
+++ src/containers/root.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {HashRouter as Router, Route} from 'react-router-dom'
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 
